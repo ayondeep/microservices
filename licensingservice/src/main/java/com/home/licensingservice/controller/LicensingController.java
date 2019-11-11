4f5bc1a915d56458e17186dd52665080b3050bd5
Ayondeep Datta <adatta@dsthealthsolutions.com>
2019-11-11 17:45:59 -0600

    test commit

--- licensingservice/src/main/java/com/home/licensingservice/controller/LicensingController.java
+++ licensingservice/src/main/java/com/home/licensingservice/controller/LicensingController.java
@@ -12,13 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class LicensingController {
 
     @Value("${example.property}")
-    private String exampleProperty;
+    private String envDesc;
 
     @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
     public License getLicenses(@PathVariable("organizationId") String organizationId,
                                @PathVariable("licenseId") String licenseId) {
 
-        System.out.println(exampleProperty); // -> Getting property value from Spring cloud config server
+        System.out.println(envDesc); // -> Getting property value from Spring cloud config server
 
         //return licenseService.getLicense(licenseId);
         return new License()
